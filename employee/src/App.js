24c40b3900339c394c3474e94dc09ff4d988bf52
Bryan Shay <forgetfulmind@gmail.com>
2021-04-06 23:22:34 -0400

    starting code

---
+++ employee/src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Search from "./pages/Search";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+        <Route exact path="/search" component={Search} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
