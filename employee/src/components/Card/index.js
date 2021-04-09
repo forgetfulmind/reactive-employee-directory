7e24b1578ddb666a28165e1419b0daf4932d7185
Bryan Shay <forgetfulmind@gmail.com>
2021-04-09 00:17:41 -0400

    adjustments to table

--- employee/src/components/Card/index.js
+++ employee/src/components/Card/index.js
@@ -14,7 +14,7 @@ console.log(props.data)
         <td>{element.name.first} {element.name.last}</td>
         <td>{element.phone}</td>
         <td>{element.email}</td>
-        <td>{element.dob.date}</td>
+        <td>{element.dob.date.split('T')[0]}</td>
       </tr>
     )
     }
