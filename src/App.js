7b31e2595817ff4330ae25bc0ac1f98f54f51e43
Bryan Shay <forgetfulmind@gmail.com>
2021-04-12 22:37:31 -0400

    trying to fix deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Search from "./pages/Search";
 import Wrapper from "./components/Wrapper";
